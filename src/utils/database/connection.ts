331edf3bfff73e9968a1db6bd6eeca511b8ac9ef
Pedro Marques <pedromarques391@gmail.com>
2025-10-13 01:35:26 -0300

    feat: create connection file to mysql.

---
+++ src/utils/database/connection.ts
@@ -0,0 +1,13 @@
+import mysql from "mysql2";
+
+export async function MySQL() {
+  const connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASS,
+  });
+
+  const pool = connection.promise();
+  return pool;
+}
