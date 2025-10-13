9bc997ece10607b9cf20bee63f8cb499ff7c1ff6
Pedro Marques <pedromarques391@gmail.com>
2025-10-13 15:07:00 -0300

    feat: create middleware to verify if jwt token is valid.

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { jwtVerify } from "jose";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  try {
+    await jwtVerify(token, secret);
+    return NextResponse.next();
+  } catch (err) {
+    console.error("[middleware] Error verifying token", err);
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/admin/addProjects/:path*"],
+};
