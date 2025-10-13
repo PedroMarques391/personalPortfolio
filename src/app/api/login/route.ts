ac5794a5e02f48f9f5820803662c14475f4189cb
Pedro Marques <pedromarques391@gmail.com>
2025-10-13 15:07:41 -0300

    feat: create jwt token and add in the cookies.

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,7 +1,9 @@
 import { MySQL } from "@/utils/database/connection";
+import * as jose from "jose";
 import { NextRequest, NextResponse } from "next/server";
-
 export async function POST(req: NextRequest) {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const alg = "HS256";
   try {
     const mysql = await MySQL();
     const { email, password } = await req.json();
@@ -15,8 +17,24 @@ export async function POST(req: NextRequest) {
         { status: 401 }
       );
     }
+
     await mysql.end();
-    return NextResponse.json(rows);
+
+    const token = await new jose.SignJWT({ email: rows[0].email })
+      .setExpirationTime("7d")
+      .setProtectedHeader({ alg })
+      .sign(secret);
+
+    const response = NextResponse.json({ success: true, user: rows[0] });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return response;
   } catch (error) {
     console.log(error);
     return NextResponse.json(error);
