e61612701cd1b30cea4626a80e25d64fc945d894
Pedro Marques <pedromarques391@gmail.com>
2025-10-13 15:00:15 -0300

    env: add new .env variable.

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,9 +1,10 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
-        USER_EMAIL: string;
-        USER_PASS: string;
-        ADDRESSEE: string;
-        MAIL_HOST: string;
-        MAIL_PORT: string;
-    }
+  interface ProcessEnv {
+    USER_EMAIL: string;
+    USER_PASS: string;
+    ADDRESSEE: string;
+    MAIL_HOST: string;
+    MAIL_PORT: string;
+    JWT_SECRET: string;
+  }
 }
