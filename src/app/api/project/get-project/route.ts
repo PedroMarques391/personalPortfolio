4c5eba6c99979bcb0c5526445e2bd7f18837f44d
Pedro Marques <pedromarques391@gmail.com>
2025-10-14 11:26:35 -0300

    feat: create api route to get all projects.

---
+++ src/app/api/project/get-project/route.ts
@@ -0,0 +1,17 @@
+import { MySQL } from "@/utils/database/connection";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const mysql = await MySQL();
+
+    const query = `SELECT * FROM projects;`;
+    const [rows]: any[] = await mysql.execute(query);
+
+    await mysql.end();
+
+    return NextResponse.json({ projects: rows }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+  }
+}
