cfd70909dff949cd06a64a578770c121e58fc899
Pedro Marques <pedromarques391@gmail.com>
2025-10-15 16:42:58 -0300

    fix: removing condition for link and removing unnecessary code.

--- src/components/UI/ProjectCard.tsx
+++ src/components/UI/ProjectCard.tsx
@@ -10,7 +10,7 @@ interface IProjectCardProps {
   type: string;
   children: React.ReactNode;
   tags: string;
-  url?: string;
+  url: string;
 }
 
 const ProjectCard = ({
@@ -25,45 +25,9 @@ const ProjectCard = ({
   const ref = useRef(null);
   const isInView = useInView(ref, { amount: 0.2 });
 
-  const CardContent = () => (
-    <>
-      <div className="relative w-full h-64">
-        <Image
-          src={src}
-          alt={title}
-          fill
-          sizes="auto"
-          priority
-          className="rounded-t-lg group-hover:scale-105 transform transition duration-300 bg-cover object-cover"
-        />
-        <span className="absolute top-3 left-3 bg-gray-light text-gray-soft text-xs font-semibold py-1 px-3 rounded-md shadow-md">
-          {type}
-        </span>
-      </div>
-
-      <div className="p-6 flex flex-col gap-2 justify-start">
-        <h2 className="text-base sm:text-lg md:text-xl  font-semibold text-gray-dark group-hover:text-gray-500 transition duration-200">
-          {title}
-        </h2>
-
-        <p className="text-gray-soft text-sm leading-relaxed">{children}</p>
-
-        <div className="flex flex-wrap gap-2 mt-2">
-          {tags.split(",").map((tag, index) => (
-            <span
-              key={index}
-              className="text-xs bg-gray-soft text-gray-light py-1 px-3 rounded-full shadow-sm"
-            >
-              #{tag}
-            </span>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-
   return (
     <motion.section
+      title={title}
       ref={ref}
       initial={{ opacity: 0.2, x: -30 }}
       animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0.2, x: -30 }}
@@ -74,23 +38,48 @@ const ProjectCard = ({
         damping: 15,
         delay: index * 0.1,
       }}
-      className={`flex flex-col w-full bg-gray-light rounded-lg shadow-lg overflow-hidden group  duration-700 ${
-        url ? "cursor-pointer" : ""
-      }`}
+      className="flex flex-col w-full bg-gray-light rounded-lg shadow-lg overflow-hidden group  duration-700 cursor-pointer"
     >
-      {url ? (
-        <Link
-          href={url}
-          prefetch
-          target="_blank"
-          rel="noopener noreferrer"
-          className="block w-full h-full"
-        >
-          <CardContent />
-        </Link>
-      ) : (
-        <CardContent />
-      )}
+      <Link
+        href={url}
+        prefetch
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block w-full h-full"
+      >
+        <section className="relative w-full h-64">
+          <Image
+            src={src}
+            alt={title}
+            fill
+            sizes="auto"
+            priority
+            className="rounded-t-lg group-hover:scale-105 transform transition duration-300 bg-cover object-cover"
+          />
+          <span className="absolute top-3 left-3 bg-gray-light text-gray-soft text-xs font-semibold py-1 px-3 rounded-md shadow-md">
+            {type}
+          </span>
+        </section>
+
+        <section className="p-6 flex flex-col gap-2 justify-start">
+          <h2 className="text-base sm:text-lg md:text-xl font-semibold text-gray-dark group-hover:text-gray-500 transition duration-200">
+            {title}
+          </h2>
+
+          <p className="text-gray-soft text-sm leading-relaxed">{children}</p>
+
+          <div className="flex flex-wrap gap-2 mt-2">
+            {tags.split(",").map((tag, index) => (
+              <span
+                key={index}
+                className="text-xs bg-gray-soft text-gray-light py-1 px-3 rounded-full shadow-sm"
+              >
+                #{tag}
+              </span>
+            ))}
+          </div>
+        </section>
+      </Link>
     </motion.section>
   );
 };
