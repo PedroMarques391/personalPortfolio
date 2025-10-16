c8e7af26c5709b6db339df128b1cdd00bf375e95
Pedro Marques <pedromarques391@gmail.com>
2025-10-16 17:46:32 -0300

    refactor: refactor ProjectCard to add isDeleting and add style whilw deleting.

--- src/components/UI/ProjectCard.tsx
+++ src/components/UI/ProjectCard.tsx
@@ -5,7 +5,6 @@ import { useRef } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 
 interface IProjectCardProps {
-  index: number;
   id: number;
   src: string;
   title: string;
@@ -14,6 +13,7 @@ interface IProjectCardProps {
   tags: string;
   url: string;
   onDelete?: (id: number) => Promise<void>;
+  isDeleting?: boolean;
 }
 
 const ProjectCard = ({
@@ -24,8 +24,8 @@ const ProjectCard = ({
   tags,
   url,
   type,
-  index,
   onDelete,
+  isDeleting,
 }: IProjectCardProps) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { amount: 0.2 });
@@ -34,69 +34,70 @@ const ProjectCard = ({
     <motion.section
       title={title}
       ref={ref}
-      initial={{ opacity: 0.2, x: -30 }}
-      animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0.2, x: -30 }}
+      initial={{ opacity: 0.2 }}
+      animate={isInView ? { opacity: 1 } : { opacity: 0.2 }}
       transition={{
-        duration: 0.6,
-        type: "spring",
-        stiffness: 80,
-        damping: 15,
-        delay: index * 0.1,
+        staggerChildren: 0.2,
+        duration: 1.5,
       }}
-      className="flex flex-col w-full bg-gray-light rounded-lg shadow-lg overflow-hidden group  duration-700 cursor-pointer"
+      className={`flex flex-col w-full bg-gray-light rounded-lg shadow-lg overflow-hidden transition-all  ease-in-out group duration-700 ${
+        isDeleting
+          ? "animate-pulse opacity-0 scale-95"
+          : "opacity-100 scale-100"
+      }`}
     >
-      <Link
-        href={url}
-        prefetch
-        target="_blank"
-        rel="noopener noreferrer"
-        className="block w-full h-full"
-      >
-        <section className="relative w-full h-64">
-          <Image
-            src={src}
-            alt={title}
-            fill
-            sizes="auto"
-            priority
-            className="rounded-t-lg group-hover:scale-105 transform transition duration-300 bg-cover object-cover"
-          />
-          <span className="absolute top-3 left-3 bg-gray-light text-gray-soft text-xs font-semibold py-1 px-3 rounded-md shadow-md">
-            {type}
-          </span>
+      <section className="relative w-full h-64 cursor-pointer">
+        <Image
+          src={src}
+          alt={title}
+          fill
+          sizes="auto"
+          priority
+          className="rounded-t-lg group-hover:scale-105 transform transition duration-300 bg-cover object-cover"
+        />
+        <span className="absolute top-3 left-3 bg-gray-light text-gray-soft text-xs font-semibold py-1 px-3 rounded-md shadow-md">
+          {type}
+        </span>
 
-          {onDelete && (
-            <button
-              onClick={async (e) => {
-                e.stopPropagation();
-                await onDelete(id);
-              }}
-              className="absolute top-3 right-3 bg-red-600 text-gray-soft text-xs font-semibold py-1 px-3 rounded-md shadow-md"
+        {onDelete && (
+          <button
+            title="Deletar Projeto"
+            onClick={async (e) => {
+              e.stopPropagation();
+              await onDelete(id);
+            }}
+            className="absolute top-3 right-3 bg-red-600 text-gray-soft text-xs font-semibold py-1 px-3 rounded-md shadow-md z-10"
+          >
+            <MdDeleteOutline color="white" size={18} />
+          </button>
+        )}
+        <Link
+          href={url}
+          prefetch
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute inset-0"
+        />
+      </section>
+
+      <section className="p-6 flex flex-col gap-2 justify-start">
+        <h2 className="text-base sm:text-lg md:text-xl font-semibold text-gray-dark group-hover:text-gray-500 transition duration-200">
+          {title}
+        </h2>
+
+        <p className="text-gray-soft text-sm leading-relaxed">{children}</p>
+
+        <div className="flex flex-wrap gap-2 mt-2">
+          {tags.split(",").map((tag, index) => (
+            <span
+              key={index}
+              className="text-xs bg-gray-soft text-gray-light py-1 px-3 rounded-full shadow-sm"
             >
-              <MdDeleteOutline color="white" size={18} />
-            </button>
-          )}
-        </section>
-
-        <section className="p-6 flex flex-col gap-2 justify-start">
-          <h2 className="text-base sm:text-lg md:text-xl font-semibold text-gray-dark group-hover:text-gray-500 transition duration-200">
-            {title}
-          </h2>
-
-          <p className="text-gray-soft text-sm leading-relaxed">{children}</p>
-
-          <div className="flex flex-wrap gap-2 mt-2">
-            {tags.split(",").map((tag, index) => (
-              <span
-                key={index}
-                className="text-xs bg-gray-soft text-gray-light py-1 px-3 rounded-full shadow-sm"
-              >
-                #{tag.trim()}
-              </span>
-            ))}
-          </div>
-        </section>
-      </Link>
+              #{tag.trim()}
+            </span>
+          ))}
+        </div>
+      </section>
     </motion.section>
   );
 };
