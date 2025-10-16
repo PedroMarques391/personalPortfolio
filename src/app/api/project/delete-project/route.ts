bb36eeed23c22d82267658f0cb104f862aed6cec
Pedro Marques <pedromarques391@gmail.com>
2025-10-16 15:51:15 -0300

    feat: create api route to delete project.

---
+++ src/app/api/project/delete-project/route.ts
@@ -0,0 +1,35 @@
+import { MySQL } from "@/utils/database/connection";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const id = searchParams.get("id");
+    const mysql = await MySQL();
+
+    const query = "DELETE FROM projects WHERE id = ?";
+
+    const [rows]: any[] = await mysql.execute(query, [id]);
+
+    if (!rows.affectedRows) {
+      throw new Error(`Não existe um projeto com o id ${id}.`);
+    }
+
+    await mysql.end();
+
+    return NextResponse.json(
+      { message: "Projeto deletado com sucesso." },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("[delete-project] Error to delete project", error.message);
+    return NextResponse.json(
+      {
+        message: error.message
+          ? error.message
+          : "Erro ao deletar projeto, tente novemente.",
+      },
+      { status: 500 }
+    );
+  }
+}
