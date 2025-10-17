4557ef7b541a2d37c4ff875d4522649756f749f3
Pedro Marques <pedromarques391@gmail.com>
2025-10-16 22:43:24 -0300

    refactor: using custom hook useProjects.

--- src/app/admin/myProjects/page.tsx
+++ src/app/admin/myProjects/page.tsx
@@ -3,8 +3,9 @@ import Skeleton from "@/components/Motions/Skeleton";
 import ProjectCard from "@/components/UI/ProjectCard";
 import ProjectsNotFound from "@/components/UI/ProjectsNotFound";
 import SectionHeader from "@/components/UI/SectionHeader";
+import useProjects from "@/hooks/useProjects";
 import { AnimatePresence } from "motion/react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export interface IProjectInterface {
   id: number;
@@ -17,78 +18,52 @@ export interface IProjectInterface {
 }
 
 const ProjectsPage = (): React.JSX.Element => {
-  const [projects, setProjects] = useState<IProjectInterface[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [deletingId, setDeletingId] = useState<number | null>(null);
+  const { projects, loading, handleDelete, deletingId, fetchProjects } =
+    useProjects();
 
-  const fetchProjects = async () => {
-    try {
-      const response = await fetch("/api/project/get-project");
-      const data = await response.json();
-      if (!data.success) return setProjects([] as IProjectInterface[]);
-      setProjects(data.projects);
-    } catch (error) {
-      console.error("[fetchProjects] error to get projects", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const fetched = useRef(false);
 
   useEffect(() => {
+    if (fetched.current) return;
+    fetched.current = true;
     fetchProjects();
   }, []);
-
-  const handleDelete = async (id: number) => {
-    try {
-      setDeletingId(id);
-      setProjects((prev) => prev.filter((p) => p.id !== id));
-      const response = await fetch(`/api/project/delete-project?id=${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
-      if (!data.success) {
-        return await fetchProjects();
-      }
-    } catch (error) {
-      console.error("[fetchProjects] error to get projects", error);
-    } finally {
-      setDeletingId(null);
-    }
-  };
 
   return (
     <div className="w-full h-full text-gray-soft flex flex-col justify-center items-center mt-10 mx-auto">
       <SectionHeader title="Projetos Cadastrados" subtitle="" />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6 mt-10 w-full">
-        <AnimatePresence>
-          {loading && (
-            <>
-              {[...Array(8)].map((_, i) => (
+        {loading ? (
+          <>
+            <AnimatePresence>
+              {[...Array(4)].map((_, i) => (
                 <Skeleton key={i} index={i} />
               ))}
-            </>
-          )}
-          {!loading && projects.length === 0 && <ProjectsNotFound />}
-          {projects.map((project, index) => (
-            <ProjectCard
-              key={index}
-              id={project.id}
-              src={project.imageURL}
-              tags={project.tags}
-              type={project.type}
-              title={project.title}
-              url={project.url}
-              onDelete={() => handleDelete(project.id)}
-              isDeleting={deletingId === project.id}
-            >
-              {project.content}
-            </ProjectCard>
-          ))}
-        </AnimatePresence>
+            </AnimatePresence>
+          </>
+        ) : (
+          <>
+            <AnimatePresence>
+              {projects.map((project, index) => (
+                <ProjectCard
+                  key={index}
+                  id={project.id}
+                  src={project.imageURL}
+                  tags={project.tags}
+                  type={project.type}
+                  title={project.title}
+                  url={project.url}
+                  onDelete={() => handleDelete(project.id)}
+                  isDeleting={deletingId === project.id}
+                >
+                  {project.content}
+                </ProjectCard>
+              ))}
+            </AnimatePresence>
+          </>
+        )}
+        {!loading && projects.length === 0 && <ProjectsNotFound />}
       </div>
     </div>
   );
