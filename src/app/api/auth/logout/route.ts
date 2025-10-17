da48ac4b484c9406570902cd42108b14fb753568
Pedro Marques <pedromarques391@gmail.com>
2025-10-17 14:39:56 -0300

    refactor: refactor logout api router.

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,21 +1,19 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
-    const cookieStore = await cookies();
+    const response = NextResponse.json({ success: true }, { status: 200 });
 
-    cookieStore.delete("token");
-
-    return NextResponse.json(
-      { success: true, message: "Logout realizado com sucesso." },
-      { status: 200 }
-    );
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 0,
+    });
+    return response;
   } catch (error: any) {
     console.error("[logout] Error to logout", error.message);
-    return NextResponse.json(
-      { success: false, message: "Erro ao fazer logout." },
-      { status: 500 }
-    );
+    return NextResponse.json({ success: false }, { status: 500 });
   }
 }
