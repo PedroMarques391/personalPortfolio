46906c9df1fa67bb9b40a841701614b5679a6cf9
Pedro Marques <pedromarques391@gmail.com>
2025-12-12 19:02:22 -0300

    refactor: separating the button values into a file.

---
+++ src/utils/projects/buttonValues.ts
@@ -0,0 +1,6 @@
+export const buttonsValues = [
+  { duration: 0.5, title: "Todos" },
+  { duration: 1.0, title: "Web" },
+  { duration: 1.5, title: "Mobile" },
+  { duration: 2.0, title: "Automações" },
+];
