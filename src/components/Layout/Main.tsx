4883693287b1b8584dfb5ece708346e5f41b0e0e
Pedro Marques <pedromarques391@gmail.com>
2025-12-16 10:13:53 -0300

    refactor: add float button into Button.tsx components.

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -2,8 +2,8 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 import ScrollLinked from "../Motions/ScrollLinked";
+import { Button } from "../UI/Button";
 import { LoadingPage } from "../UI/LoadingPage";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -49,18 +49,7 @@ const Main = ({ children }: IBodyProps): React.JSX.Element => {
               {children}
             </QueryClientProvider>
           </div>
-          <button
-            className={`w-10 h-10 sm:h-12 sm:w-12 bg-white/30 justify-center items-center flex fixed bottom-10 right-5 md:right-10 rounded-xl duration-500 transition-opacity ${
-              scrollY ? "opacity-1" : "opacity-0"
-            }`}
-          >
-            <MdKeyboardDoubleArrowUp
-              aria-label="Scroll To Top"
-              className="text-4xl md:text-6xl"
-              color="#1c1c1c"
-              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            />
-          </button>
+          <Button.Float scrollY={scrollY} />
           <Footer />
         </main>
       )}
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -2,66 +2,105 @@
 import { motion, useInView } from "motion/react";
 import Link from "next/link";
 import { useRef } from "react";
+import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 
-interface IButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    children: React.ReactNode;
-    styles?: string;
-    duration?: number
+interface IButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  children: React.ReactNode;
+  styles?: string;
+  duration?: number;
 }
 
-interface IButtonFooterProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    children: React.ReactNode;
-    href: string
-    duration?: number
+interface IButtonFooterProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  children: React.ReactNode;
+  href: string;
+  duration?: number;
 }
 
-function Button({ children, styles, duration = 0.5, ...props }: IButtonProps): React.JSX.Element {
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: -10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: duration, ease: "easeInOut" }}
-        >
-            <button
-                {...props}
-                className={`p-4 border border-transparent flex gap-2 text-sm items-center justify-center rounded-3xl bg-gray-light w-40 
+function Button({
+  children,
+  styles,
+  duration = 0.5,
+  ...props
+}: IButtonProps): React.JSX.Element {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: duration, ease: "easeInOut" }}
+    >
+      <button
+        {...props}
+        className={`p-4 border border-transparent flex gap-2 text-sm items-center justify-center rounded-3xl bg-gray-light w-40 
                 hover:border-orange-400 hover:scale-110 hover:text-orange-400 transform origin-center z-10 duration-300 ease-in-out ${styles}`}
-            >
-                {children}
-            </button>
-        </motion.div>
-    );
-};
+      >
+        {children}
+      </button>
+    </motion.div>
+  );
+}
 
-function Footer({ children, duration, href }: IButtonFooterProps): React.JSX.Element {
-    const ref = useRef<HTMLParagraphElement>(null);
-    const isInView = useInView(ref, { amount: 0.1 });
+function Footer({
+  children,
+  duration,
+  href,
+}: IButtonFooterProps): React.JSX.Element {
+  const ref = useRef<HTMLParagraphElement>(null);
+  const isInView = useInView(ref, { amount: 0.1 });
 
-    const itemVariant = {
-        hidden: { opacity: 0, scale: 0 },
-        visible: { opacity: 1, scale: 1 },
-    };
+  const itemVariant = {
+    hidden: { opacity: 0, scale: 0 },
+    visible: { opacity: 1, scale: 1 },
+  };
 
-    return (
-        <Link
-            href={href}
-            prefetch
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block py-2 text-base md:text-xl tracking-wider  text-gray-dark hover:text-gray-soft text-center px-2 hover:-translate-x-10 transition-transform duration-200"
-            aria-label={`Abrir link ${children} em uma nova aba`}
-        >
-            <motion.p
-                ref={ref}
-                initial="hidden"
-                animate={isInView ? "visible" : "hidden"}
-                variants={itemVariant}
-                transition={{ duration: duration, }}
-            >{children}</motion.p>
-        </Link>
-    );
+  return (
+    <Link
+      href={href}
+      prefetch
+      target="_blank"
+      rel="noopener noreferrer"
+      className="block py-2 text-base md:text-xl tracking-wider  text-gray-dark hover:text-gray-soft text-center px-2 hover:-translate-x-10 transition-transform duration-200"
+      aria-label={`Abrir link ${children} em uma nova aba`}
+    >
+      <motion.p
+        ref={ref}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        variants={itemVariant}
+        transition={{ duration: duration }}
+      >
+        {children}
+      </motion.p>
+    </Link>
+  );
+}
+
+function Float({ scrollY }: { scrollY: boolean }): React.JSX.Element {
+  return (
+    <button
+      className={`w-10 h-10 sm:h-12 sm:w-12 bg-white/30 justify-center items-center flex fixed bottom-10 right-5 md:right-10 rounded-xl duration-500 transition-opacity ${
+        scrollY ? "opacity-1" : "opacity-0"
+      }`}
+    >
+      <MdKeyboardDoubleArrowUp
+        aria-label="Scroll To Top"
+        className="text-4xl md:text-6xl"
+        color="#1c1c1c"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      />
+    </button>
+  );
 }
 
 Button.Footer = Footer;
+
+Button.Float = Float;
 
 export { Button };
