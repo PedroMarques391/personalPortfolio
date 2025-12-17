10b31de97a94992152406dae3b4d1db7135b3079
Pedro Marques <pedromarques391@gmail.com>
2025-12-16 23:37:28 -0300

    fix: add query parans in projects url  from desktop url.

--- src/components/Menu/Desktop.tsx
+++ src/components/Menu/Desktop.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { NavItems } from './NavItems';
+import React from "react";
+import { NavItems } from "./NavItems";
 
 const Desktop = (): React.JSX.Element => {
-    return (
-        <ul className="hidden md:flex justify-center items-center gap-x-3">
-            <NavItems href="/">Home</NavItems>
-            <NavItems href="/projects">Projetos</NavItems>
-            <NavItems href="/about">Sobre Mim</NavItems>
-            <NavItems href="/contact">Contato</NavItems>
-        </ul>
-    );
+  return (
+    <ul className="hidden md:flex justify-center items-center gap-x-3">
+      <NavItems href="/">Home</NavItems>
+      <NavItems
+        href={{ pathname: "/projects", query: { page: 1 }, scroll: false }}
+      >
+        Projetos
+      </NavItems>
+      <NavItems href="/about">Sobre Mim</NavItems>
+      <NavItems href="/contact">Contato</NavItems>
+    </ul>
+  );
 };
 
 export default Desktop;
