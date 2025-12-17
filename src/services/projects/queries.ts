c9881d268750e3373e0c1c81e4dcdab3087c5c86
Pedro Marques <pedromarques391@gmail.com>
2025-12-16 23:24:00 -0300

    feat: add page in api router to config pagination.

--- src/services/projects/queries.ts
+++ src/services/projects/queries.ts
@@ -1,14 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { Requests } from "../requests";
 
-export function useProjects(filter: "all" | "user-projects") {
+export function useProjects(filter: "all" | "user-projects", page: number = 1) {
   return useQuery({
-    queryKey: ["projects", filter],
-
+    queryKey: ["projects", filter, page],
     queryFn: async () => {
-      return await Requests.getProject(`/api/project?role=${filter}`);
+      return await Requests.getProject(
+        `/api/project?role=${filter}&page=${page}`
+      );
     },
     staleTime: 1000 * 60 * 60,
     refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData,
   });
 }
