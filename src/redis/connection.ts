95d0e6d4740b23af2dbb6d3169e8a7cd2f089913
Pedro Marques <pedromarques391@gmail.com>
2026-05-18 23:21:11 -0300

    fix: edit url .env

--- src/redis/connection.ts
+++ src/redis/connection.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 const redis = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL_REDIS_URL || "redis://localhost:6379",
 })
   .on("error", (err) => console.error("Redis Client Error", err))
   .on("connect", () => console.log("Connected to Redis"));
